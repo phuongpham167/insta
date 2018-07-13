efb6bb17a9808207461d686da5df87296ce62e7c
Phuong Pham <phuongptt.uet@gmail.com>
2018-07-13 14:06:54 +0700

    abc

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module KenGram
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
